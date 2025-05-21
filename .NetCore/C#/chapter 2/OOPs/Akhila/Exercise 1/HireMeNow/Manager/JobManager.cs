292299b711b986c56a8528345964d6f6b54ff9f4
Akhila <akhilarithin@gmail.com>
2025-05-21 16:23:24 +0530

    OOPs exercise1 added

---
+++ .NetCore/C#/chapter 2/OOPs/Akhila/Exercise 1/HireMeNow/Manager/JobManager.cs
@@ -0,0 +1,54 @@
+﻿using HireMeNow.Models;
+using HireMeNow.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HireMeNow.Interface;
+
+namespace HireMeNow.Manager
+{
+    public class JobManager
+    {
+        public Job[] jobs = new Job[]{new Job(1, "SE",ExperienceLevels.Fresher,"Dell","Pune","25k","Programmer",0),
+                              new Job(2,"WebDeveloper",ExperienceLevels.MidLEvel,"Accenture","Kochi","35k","Developer",0),
+                               new Job(3,"WebDeveloper",ExperienceLevels.MidLEvel,"Accenture","Kochi","35k","Developer", 0),
+                               new Job(4, "SE",ExperienceLevels.Fresher,"Dell","Pune","25k","Programmer", 0),
+                              new Job(5,"WebDeveloper",ExperienceLevels.MidLEvel,"E&Y","Kochi","35k","Developer", 0),
+                               new Job(6,"WebDeveloper",ExperienceLevels.MidLEvel,"Accenture","Kochi","35k","Developer", 0),
+                               new Job(7, "SE",ExperienceLevels.Fresher,"Dell","Pune","25k","Programmer", 0),
+                              new Job(8,"WebDeveloper",ExperienceLevels.MidLEvel,"TCS","Kochi","35k","Developer", 0),
+                               new Job(9,"WebDeveloper",ExperienceLevels.MidLEvel,"Accenture","Kochi","35k","Developer", 0),
+                               new Job(10,"Backend Developer",ExperienceLevels.Senior,"Infosys","TVM","50k","Developer", 0)
+
+        };
+
+
+        //Methods
+        public void ListJobs()
+        {
+            Console.WriteLine("List of Available Jobs:");
+            for (int i = 0; i < jobs.Length; i++)
+            {
+                Console.WriteLine($"JobId:{jobs[i].Id}   Title:{jobs[i].Title}   Exp.Level:{jobs[i].ExperienceLevels.ToString()}   Company:{jobs[i].Company}   Location:{jobs[i].Location}   SalaryRange:{jobs[i].SalaryRange}   JobType:{jobs[i].JobType}");
+            }
+        }
+
+
+        public void GetJobbyId(int id)
+        {
+            foreach(Job job in jobs)
+            {
+                if(job.Id == id)
+                {
+                    Console.WriteLine($"Details of job:\nJobTitle:{job.Title}\nCompany:{job.Company}\nJob Location:{job.Location}\n Salary:{job.SalaryRange}");
+                }
+                
+            }
+        }
+
+
+
+    }
+}
---
+++ .NetCore/C#/chapter 2/OOPs/Akhila/Exercise 1/HireMeNow/Models/JobSeeker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using HireMeNow.Enums;
+
+namespace HireMeNow.Models
+{
+    public class JobSeeker
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Location { get; set; }
+        public string AboutMe { get; set; }
+        public string Qualification { get; set; }
+        public ExperienceLevels ExperienceLevel { get; set; }
+        public string Password { get; set; }
+
+        public JobSeeker() { }
+        public JobSeeker(string email,string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public JobSeeker(int id, string firstName, string lastName, string email, string phone, string location, string aboutMe, string qualification, ExperienceLevels experienceLevel, string password)
+        {
+            Id= id;
+            FirstName= firstName;
+            LastName= lastName;
+            Email= email;
+            Phone= phone;
+            Location= location;
+            AboutMe= aboutMe;
+            Qualification= qualification;
+            ExperienceLevel= experienceLevel;
+            Password= password;
+        }
+    }
+}
