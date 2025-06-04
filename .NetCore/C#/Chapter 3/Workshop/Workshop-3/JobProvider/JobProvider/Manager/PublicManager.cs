701832cb62427b9a96d1bb710f32639a627405d3
reeshma <reeshma.aitrich@gmail.com>
2025-06-04 16:38:58 +0530

    added

---
+++ .NetCore/C#/Chapter 3/Workshop/Workshop-3/JobProvider/JobProvider/Manager/PublicManager.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Admin_Job.Manager;
+using JobProvider.Enums;
+using JobProvider.Exceptions;
+using JobProvider.Interface;
+using JobProvider.Models;
+using JobProvider.Repository;
+using System.Text.RegularExpressions;
+
+namespace JobProvider.Manager
+{
+    public class PublicManager : IMenu
+    {
+        private User loggedUser;
+
+        public PublicManager(User loggedUser)
+        {
+            this.loggedUser = loggedUser;
+        }
+        public PublicManager()
+        {
+
+        }
+
+        public void DisplayMenu()
+        {
+            
+        }
+    }
+}
