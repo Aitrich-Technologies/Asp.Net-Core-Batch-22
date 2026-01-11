4ddbb4e65c3fb1f1e605a1e024a3fc57c8348872
AkhilaRithin <akhilarithin@gmail.com>
2026-01-11 19:53:20 +0530

    ProjectStructure Added

---
+++ .NetCore/JobPortal_Project/Domain/Service/AuthUser/AuthUserService.cs
@@ -0,0 +1,23 @@
+﻿
+using Domain.Models;
+using Domain.Service.Authuser.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Service.Authuser
+{
+	public class AuthUserService:IAuthUserService
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly IAuthUserRepository _userRepository;
+
+		public AuthUserService(IHttpContextAccessor httpContextAccessor,IAuthUserRepository userRepository)
+		{
+			_httpContextAccessor = httpContextAccessor;
+			_userRepository = userRepository;
+		}
+
+        
+
+        
+    }
+}
---
+++ .NetCore/JobPortal_Project/JobPortal_Project/Extensions/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿
+
+using AutoMapper;
+using Domain.Models;
+
+
+namespace HireMeNow_WebApi.Extensions
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+          
+        }
+    }
+}
