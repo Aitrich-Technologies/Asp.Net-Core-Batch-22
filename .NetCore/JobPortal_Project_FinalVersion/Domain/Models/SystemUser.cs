662369a49022fc6593cd14394a74fbdcb974c043
Neethu <neethukottoor@gmail.com>
2026-02-03 21:50:12 +0400

    finalproject

---
+++ .NetCore/JobPortal_Project_FinalVersion/Domain/Models/SystemUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models;
+
+public partial class SystemUser
+{
+    public Guid Id { get; set; }= Guid.NewGuid();
+
+    public string? UserName { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; }
+
+    public string Phone { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public Enums.Role Role { get; set; }
+    public virtual JobSeeker? JobSeeker { get; set; }
+
+
+}
