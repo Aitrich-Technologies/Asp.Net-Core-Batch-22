f7f4d465715f2b438220df489463ee67123544c2
Neethu <neethukottoor@gmail.com>
2026-02-07 16:04:55 +0400

    deleted

--- .NetCore/JobPortal_Project_FinalVersion/JobPortal_Project/API/JobProvider/RequestObjects/InterviewSheduleObject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JobPortal_Project.API.JobProvider.RequestObjects
-{
-    public class InterviewSheduleObject
-    {
-        public Guid? ApplicationId { get; set; }
-        public DateTime? Date { get; set; }
-    }
-}
